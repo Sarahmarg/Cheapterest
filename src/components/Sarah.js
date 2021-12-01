32a89379f19a5e21867068aad13158cdc63d41c9
nathael-lab <nathael.labat@mmibordeaux.com>
2021-12-01 14:44:09 +0100

    v 0.9

---
+++ src/components/Sarah.js
@@ -0,0 +1,15 @@
+import Header from "./Header/header"
+import PostsPage from "../pages/Posts"
+
+const Sarah = () => {
+    return (
+        <div>
+            <Header/>
+            <div className="App"> 
+                <PostsPage />
+            </div>
+        </div>
+    )
+}
+
+export default Sarah
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import { getByText } from '@testing-library/dom';
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom';
+
+function Homepage(props) {
+
+    console.log(props)
+}
+
+export default Homepage
