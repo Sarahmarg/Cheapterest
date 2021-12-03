a11a40a4a772a624e7dbbb2bd96f958dfb8ca946
Sarah Marguerettaz <sarah.marg47@gmail.com>
2021-12-03 15:09:38 +0100

    TchipterestV2

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -4,15 +4,15 @@ import styles from '../components/Header/Header.module.css'
 
 //Publier un post
 function PostsPage(props) {
-    console.log (props.onSubmit)
+    console.log(props.onSubmit)
 
 
     //Definir les composant de l'url, du titre et de la description
     const [url, setUrl] = useState()
     const [image, setImage] = useState()
-    const [title, setTitle] = useState ()
-    const[description, setDescription] = useState ()
-    const [category, setCategory]= useState('montagne')
+    const [title, setTitle] = useState()
+    const [description, setDescription] = useState()
+    const [category, setCategory] = useState('montagne')
     const [item, setItem] = useState([])
 
     //Prendre les champs rédigé par l'utilisateur et les définir en valeurs
@@ -22,23 +22,23 @@ function PostsPage(props) {
     }
 
     const handChangeTitle = (event) => {
-        setTitle (event.target.value)
+        setTitle(event.target.value)
     }
 
     const handChangeDescription = (event) => {
-        setDescription (event.target.value)
+        setDescription(event.target.value)
     }
 
     const handChangeCategory = (event) => {
         switch (event.target.value) {
             case "montagne":
-                setCategory ("montagne")
+                setCategory("montagne")
                 break
             case "nature":
-                setCategory ("nature")
+                setCategory("nature")
                 break
             case "ville":
-                setCategory ("ville")
+                setCategory("ville")
                 break
             default:
                 console.log('catégorie inconnue.')
@@ -49,17 +49,17 @@ function PostsPage(props) {
     const addItemandClean = () => {
         // setItem([...item, {url,title,description}])
         props.onSubmit({
-            url: url, 
-            title: title, 
+            url: url,
+            title: title,
             description: description,
             category: category
-            
+
         })
         setImage('')
         setUrl('')
         setDescription('')
         setTitle('')
-    
+
     }
 
     const clean = () => {
@@ -74,29 +74,31 @@ function PostsPage(props) {
     return (
         <div className="container-addpost">
             <label className='url-input'>Url de l'image : </label>
-            <input onChange={handChangeUrl} type="text" value={url}></input><br/>
-            
-            <img className='introuvable-input'src={url} alt="introuvable"></img><br/>
+            <input onChange={handChangeUrl} type="text" value={url}></input><br />
+
+            <img className='introuvable-input' src={url} alt="introuvable"></img><br />
 
             <label className='titre-input'>Titre : </label>
-            <input onChange={handChangeTitle} type="text" value={title}></input><br/>
+            <input onChange={handChangeTitle} type="text" value={title}></input><br />
 
             <label className='description-input'>Description : </label>
-            <input onChange={handChangeDescription} type="text" value ={description}></input>
-            
-            <select onChange={handChangeCategory}>
+            <input onChange={handChangeDescription} type="text" value={description}></input>
 
-                <option value="montagne">Montagne</option>
-                <option value="nature">Nature</option>
-                <option value="ville">Ville</option>
-            </select>
-            <br/>
-            <br/>
+            <div className={styles.category}>
+                <select onChange={handChangeCategory}>
+
+                    <option value="montagne">Montagne</option>
+                    <option value="nature">Nature</option>
+                    <option value="ville">Ville</option>
+                </select>
+            </div>
+            <br />
+            <br />
 
             <button className={styles.button} onClick={() => addItemandClean()}>Publier</button>
-            <button className={styles.button} onClick={() => clean() }>Réinitialiser</button>
+            <button className={styles.button} onClick={() => clean()}>Réinitialiser</button>
 
-        
+
         </div>
     )
 }
