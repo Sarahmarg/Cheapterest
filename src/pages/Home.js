9676c4fd1fc64233b019e5e7b7518120296f4c14
Sarah Marguerettaz <sarah.marg47@gmail.com>
2021-12-03 14:52:56 +0100

    CssSarahV25

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,56 @@
 
 import { useState } from "react";
-import PostsPage from "./Posts";
+import styles from "./Home.module.css"
 
 //Page d'accueil
 
+
+//afficher les posts et les filtrer
 const Nath = (props) => {
 
-    const [categoryFilter, setCategoryFilter] = useState('')
+  const [categoryFilter, setCategoryFilter] = useState('')
 
-    console.log('allPost', props.posts)
-    console.log(props.postFiltered)
+  console.log('allPost', props.posts)
+  console.log(props.postFiltered)
 
-    const itemsFilter = props.posts.filter((post) => {
-      if (categoryFilter) {
-        return post.category === categoryFilter
-      }
-      return true
-    })
- 
-    return (
-      <div>
+  const itemsFilter = props.posts.filter((post) => {
+    if (categoryFilter) {
+      return post.category === categoryFilter
+    }
+    return true
+  })
+  //Les filtres de category
+  return (
+    <div >
+      <div className={styles.category}>
         <select onChange={(event) => setCategoryFilter(event.target.value)}>
           <option value="">Voir tous</option>
           <option value="montagne">Montagne</option>
           <option value="nature">Nature</option>
           <option value="ville">Ville</option>
         </select>
-        {itemsFilter.length === 0 ? (
-          <span>Pas de post</span> 
-        ) : (
-          <div>
-              {itemsFilter.map(function (item) {
-                return (
-                  <div>
-                    <div className ={''}>{item.title}</div>
-                    <div>{item.description}</div>
-                    <div><img src={item.url} alt='introuvable'></img></div>
-                  </div>
-                )
-              })}
-          </div>
-        )}
       </div>
-    )
+      <br />
+      <br />
+      {itemsFilter.length === 0 ? (
+        <div className={styles.span}>
+          <span >Pas de post</span>
+        </div>
+      ) : (
+        <div>
+          {itemsFilter.map(function (item) {
+            return (
+              <div className={styles.post}>
+                <div className={styles.title}>{item.title}</div>
+                <div ><img className={styles.image} src={item.url} alt='introuvable'></img></div>
+                <div className={styles.description}>{item.description}</div>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
 }
 
 
